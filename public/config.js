a8bdd82a22866da2521ef7cef259c353956037a7
wangp <wangp@infcn.com.cn>
2018-06-27 14:51:59 +0800

    第一次提交

---
+++ public/config.js
@@ -0,0 +1,4 @@
+var config = {
+	apiPath: 'http://192.168.10.58:8085/api/'
+	// apiPath: 'http://lysys.infcn.com.cn/api/'
+};
---
+++ src/mobx/semantic-association-store.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Administrator on 2017/7/11.
+ */
+import {observable, runInAction, computed, action, reaction, autorun} from "mobx";
+import LynlpApi from "../common/lynlp-api"
+
+
+class SemanticAssociationStore {
+
+	@observable fetching = true;
+	@observable fetchingTu = true;
+	@observable changeCurrent = 0;
+	@observable recommend = {};
+	@observable current = 0;
+	@observable graph = {};
+	@observable keyItem = '';
+
+
+	@action
+	fetchData(content) {
+		this.current = 0
+		this.fetching = true;
+		this.recommend = {};
+		LynlpApi.semanticRecommend(content).then(result => {
+			let Key = _.keys(result);
+			this.keyItem = "" + Key[0];
+			this.changeCurrent++;
+			for (var i in result) {
+				this.recommend[i] = result[i];
+			}
+			this.fetching = false;
+			this.fetchingTu = true;
+			LynlpApi.semanticRecommendGraph(Key[0]).then(result => {
+				this.graph = result;
+				this.fetchingTu = false;
+			});
+		});
+
+	}
+
+	@action
+	fetchDataGraph(keyword) {
+		this.fetchingTu = true;
+		this.graph = {};
+		LynlpApi.semanticRecommendGraph(keyword).then(result => {
+			this.graph = result;
+			this.fetchingTu = false;
+		});
+
+	}
+
+}
+
+const semanticAssociationStore = new SemanticAssociationStore();
+export default semanticAssociationStore
+
+
